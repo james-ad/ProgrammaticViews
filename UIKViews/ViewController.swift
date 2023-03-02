29a024cac4a20c9ee49eed94fc6ed5cc91f12681
james-du <muzikjay@gmail.com>
2023-03-01 20:21:44 -0800

    Get first button on page after setting window scene and root view controller

--- UIKViews/ViewController.swift
+++ UIKViews/ViewController.swift
@@ -8,12 +8,87 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let button = UIButton()
+//    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+        setupButton()
     }
-
-
+    
+    func setupButton() {
+        view.addSubview(button)
+        
+        button.configuration = .filled()
+        button.configuration?.baseBackgroundColor = .systemMint
+        button.configuration?.title = "Hello, World"
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 200),
+            button.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
 }
 
+//class TableViewDelly: UITableViewDataSource, UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+//
+//    func isEqual(_ object: Any?) -> Bool {
+//        <#code#>
+//    }
+//
+//    var hash: Int = 0
+//
+//    var superclass: AnyClass?
+//
+//    func `self`() -> Self {
+//        <#code#>
+//    }
+//
+//    func perform(_ aSelector: Selector!) -> Unmanaged<AnyObject>! {
+//        <#code#>
+//    }
+//
+//    func perform(_ aSelector: Selector!, with object: Any!) -> Unmanaged<AnyObject>! {
+//        <#code#>
+//    }
+//
+//    func perform(_ aSelector: Selector!, with object1: Any!, with object2: Any!) -> Unmanaged<AnyObject>! {
+//        <#code#>
+//    }
+//
+//    func isProxy() -> Bool {
+//        <#code#>
+//    }
+//
+//    func isKind(of aClass: AnyClass) -> Bool {
+//        <#code#>
+//    }
+//
+//    func isMember(of aClass: AnyClass) -> Bool {
+//        <#code#>
+//    }
+//
+//    func conforms(to aProtocol: Protocol) -> Bool {
+//        <#code#>
+//    }
+//
+//    func responds(to aSelector: Selector!) -> Bool {
+//        <#code#>
+//    }
+//
+//    var description: String = ""
+//
+//
+//}
